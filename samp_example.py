4147c1de94e4cdb38e10fc2c6d959cd2b60b43e0
Nico Vermaas <vermaas@astron.nl>
2021-05-27 11:40:10 +0200

    add samp_example

--- samp_example.py
+++ samp_example.py
@@ -1,4 +1,8 @@
 from shopping_client import shopping_client
 
+# this is very raw.
+# Officially it doesn't work because a AAI token is required,
+# but in 'development mode' that part is skipped in the backend, so that the results are returned anyway
+# A proper client should log in with AAI though.
 client = shopping_client(username="vermaas",host="http://localhost:5555/")
 client.get_basket()
